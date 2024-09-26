eb0bb670c3df2b7d21c5488c5809a1f6bc401662
vp-nocode <166547991+vp-nocode@users.noreply.github.com>
2024-09-26 22:59:27 +0500

    mod main.py

--- main.py
+++ main.py
@@ -13,29 +13,49 @@ async def main():
 
 @dp.message(CommandStart())
 async def start(message: Message):
-    await message.answer("Hi, I'm a bot!")
+    await message.answer(f"Hi, {message.from_user.first_name} ({message.from_user.full_name}), I'm a bot!")
 
 @dp.message(Command('help'))
-async def help(message: Message):
+async def command_help(message: Message):
     await message.answer("This bot can execute the commands:\n /start\n /help")
 
-@dp.message(F.text == "Что такое ИИ?")
+@dp.message(F.text == "What is AI?")
 async def aitext(message: Message):
-    await message.answer('Искусственный интеллект — это свойство искусственных интеллектуальных систем выполнять творческие функции, которые традиционно считаются прерогативой человека; наука и технология создания интеллектуальных машин, особенно интеллектуальных компьютерных программ')
+    await message.answer('Artificial intelligence is the ability of artificial intelligent systems to perform creative'
+                         ' functions traditionally considered the prerogative of humans; the science and technology '
+                         'of creating intelligent machines, especially intelligent computer programs ')
 
 @dp.message(F.photo)
 async def react_photo(message: Message):
-    list = ['Ого, какая фотка!', 'Непонятно, что это такое', 'Не отправляй мне такое больше']
-    rand_answer = random.choice(list)
+    message_list = ['Wow, what a photo!', 'I don\'t understand what this is', 'Don\'t send me that again']
+    rand_answer = random.choice(message_list)
     await message.answer(rand_answer)
+    await bot.download(message.photo[-1], destination=f'tmp/{message.photo[-1].file_id}.jpg')
 
-@dp.message(Command('photo'))
+@dp.message(Command('photo', prefix='&'))
 async def photo(message: Message):
-    list = ['https://funny.klev.club/uploads/posts/2024-03/funny-klev-club-p-smeshnie-kartinki-detskie-dlya-podnyatiya-13.jpg',
+    photo_list = ['https://funny.klev.club/uploads/posts/2024-03/funny-klev-club-p-smeshnie-kartinki-detskie-dlya-podnyatiya-13.jpg',
             'https://funny.klev.club/uploads/posts/2024-03/funny-klev-club-p-smeshnie-kartinki-detskie-dlya-podnyatiya-12.jpg',
             'https://funny.klev.club/uploads/posts/2024-03/funny-klev-club-p-smeshnie-kartinki-detskie-dlya-podnyatiya-7.jpg']
-    rand_photo = random.choice(list)
+    rand_photo = random.choice(photo_list)
     await message.answer_photo(photo=rand_photo, caption='Cool picture!')
+
+@dp.message()
+async def start(message: Message):
+    if message.text.lower() == 'test':
+        await message.answer('Testing')
+
+@dp.message()
+async def start(message: Message):
+    await message.answer("Sorry, I didn't understand that command or message. Please try again.")
+
+'''
+# echo-bot
+@dp.message()
+async def start(message: Message):
+    await message.send_copy(chat_id=message.chat.id)
+'''
+
 
 if __name__ == "__main__":
     asyncio.run(main())
